7052cd6e645cc24acc3a60c647a1c0bf2c5f89fe
lecoh2 <lecoh2@hotmail.com>
2026-02-11 13:13:00 -0300

    criação dos controles de categoria e movimentacao

---
+++ FinApp.Api/Controllers/V1/CategoriasController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinApp.Api.Controllers.V1
+{
+    [Route("api/v1/categorias")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> PostAsync()
+        {
+            throw new NotImplementedException();
+        }
+        [HttpPut]
+        public async Task<IActionResult> PutAsync()
+        {
+            throw new NotImplementedException();
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteAsync()
+        {
+            throw new NotImplementedException();
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
